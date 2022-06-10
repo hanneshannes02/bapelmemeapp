12ef3aad7b71d9ff282f45461cccd5756826df0d
hanneshannes02 <hanneshainbucher@gmail.com>
2022-06-10 11:45:06 +0200

    gitupdatetest

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,12 +1,5 @@
-import type { ReactNode } from 'react';
-import React from 'react';
-import classes from './Button.module.css';
+//import type { ReactNode } from 'react';
+//import React from 'react';
+//import styles from  "/home/dev/bapelmemeapp/src/app/App.module.css"
 
-type ButtonProps = {
-  children: ReactNode;
-};
-function Button({ children }: ButtonProps): JSX.Element {
-  return <button className={classes.button}>{children}</button>;
-}
 
-export default Button;
