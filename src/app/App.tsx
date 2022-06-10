b467adf168bcdd9f96ff1a2b1b55f647b35f9267
hanneshannes02 <hanneshainbucher@gmail.com>
2022-06-10 12:46:15 +0200

    test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,65 +1,45 @@
-import React, {} from 'react'; /* useState war drinnen in {} */
-import styles from "./App.module.css";
-import Image from "/home/dev/bapelmemeapp/src/app/Images/IT Bild.jpg";
+import React from 'react'; /* useState war drinnen in {} */
+import styles from './App.module.css';
+import Image from '/home/dev/bapelmemeapp/src/app/Images/IT Bild.jpg';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App(): JSX.Element {
-
-
-
   return (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/loggingin">
-    
-        <h1>Hi</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/loggingin">
+          <h1>Hi</h1>
+        </Route>
 
-      </Route>
+        <Route path="/global">
+          <section>
+            <div className={styles.header}>
+              <h1>Willkommen hier auf Omnius.one</h1>
+            </div>
+          </section>
+        </Route>
 
+        <Route path="/">
+          <body>
+            <section className={styles.main}>
+              <div className={styles.top}>
+                <h1 className={styles.Omnius}>OMNIUS.ONE</h1>
 
+                <div className="wrapper">
+                  <a href="loggingin">
+                    <span>LOGGING IN</span>
+                  </a>
+                  <a href="global">
+                    <span>GLOBAL</span>
+                  </a>
+                </div>
+              </div>
 
-      <Route path="/global">
-       <section>
-         
-       <div className={styles.header}>
-
-          <h1>Willkommen hier auf Omnius.one</h1>
-
-        </div>
-         
-         </section> 
-
-        
-
-      </Route>
-
-
-
-      <Route path="/">
-
-        <body>
-         <section className={styles.main}>
-
-          <div className={styles.top}>
-
-            <h1 className={styles.Omnius}>OMNIUS.ONE</h1>
-
-             <div className="wrapper">
-               <a href="loggingin"><span>LOGGING IN</span></a>
-               <a href="global"><span>GLOBAL</span></a>
-             </div>
-          
-          </div>
-
- 
-          <img className={styles.image} src={Image} />
- 
-         </section>
-        </body>
-      </Route>
-     </Switch>
-</BrowserRouter>
-
-
+              <img className={styles.image} src={Image} />
+            </section>
+          </body>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 export default App;
